fa76c427cb8edaec4174617aa801e771f13320cb
Bennett <bennettcheung@yahoo.com>
2018-08-20 12:27:17 -0700

    completed part 4

--- W4D1_Playground.playground/Pages/Optionals.xcplaygroundpage/Contents.swift
+++ W4D1_Playground.playground/Pages/Optionals.xcplaygroundpage/Contents.swift
@@ -19,13 +19,14 @@ var doubleNumberValue: Double? = nil
  - Experiment:
  Declare a non-optional variable of a `String` and set it to `nil`. What happens?
  */
-
+var someVar = "String"
 
 /*:
  - Experiment:
  Declare an optional variable of a type `String` and set an initial `String` value to it. Then set it to `nil` on the next line. Does this work? Why or why not?
  */
-
+var someVar2: String? = "string value"
+someVar2 = nil
 
 /*:
  Let's consider multiplying two numbers together that are of different types. Since we have a `Double` and an `Int`, we need to convert the two numbers to the same type. For example, creating a new `Double` using an `Int`.
@@ -47,14 +48,17 @@ ratio * convertIntegerValue // now this works!
  - Experiment:
  Declare a `String` containing digits and try converting it to a `Double` the same way shown in the above example. What do you notice about the variable type? Hint: Use 'Option' + Mouse Click on the variable to see the type
  */
-
+var digitString = "20"
+var number:Double? = Double(digitString)
 
 /*:
  - Experiment:
  With your newly converted `Double` from a `String`, try multiplying it with the 'ratio' variable. What happens?
  */
-
-
+if let number = number{
+ let result = number * ratio
+  print (result)
+}
 /*:
  Your newly converted `Double` value is a `Double?` which indicates we might have a double or we might have nothing.  Converting a `String` to a `Double` might fail because the `String` does not guarantee there will only be digits within it.
  
@@ -77,7 +81,7 @@ print("\(myOptionalDouble!)")
   - Experiment:
  Now you try! Try printing out your converted `Double?` with a force unwrap
 */
-
+print (number!)
 
 
 /*:
@@ -85,20 +89,23 @@ print("\(myOptionalDouble!)")
  Go back and change your `String` to something that has no digits. What happens and why?
  */
 
-
-
+//digitString = "sef"
+//number = Double(digitString)
+//print (number)
 /*:
  - Experiment:
  Declare an optional variable of a type `String` and set an initial `String` value to it. Try printing it.
  Now print it again, but this time unwrap the optional variable using the `'!'`. What's different about the two lines you printed?
  */
-
+let someString: String? = "some string"
+print (someString!)
 
 /*:
  - Experiment:
  Try setting an optional `String` variable to a non-optional `String` variable. What happens? What can you do to prevent the compiler from throwing an error?
  */
-
+//let someString2 = "Test"
+//let someString = someString2
 
 /*:
  The next way to deal with optionals is called `"Conditional unwrapping"` or sometimes casually called an `"if-let"`. It's **much** safer, and won't break your Playground, or any of your code.
@@ -121,19 +128,20 @@ if let unwrapped = gravityConstant {
  - Experiment:
  Try changing `gravityConstant` to nil. What happens?
  */
-
+//gravityConstant = nil won't let it because it is a constant
 
 /*:
  - Experiment:
  Create an array with containing elements of any type of your choice. Try experimenting with the array methods `'first'` and `'last'` to find out what they do. You'll see that both return optional values. Print out the values of first and last by using conditional unwrapping.
  */
-
-
+let cities = ["Vancouver", "Toronto", "Montreal"]
+let firstCity = cities.first
+let lastCity = cities.last
 /*:
  - Experiment:
  Using the same array, experiment with the array method `'indexOf'` and find out what it does. Print out the value using conditional unwrapping.
  */
-
+cities.index(of: "Vancouver")
 
 /*:
  - Callout(Challenge):
@@ -150,3 +158,10 @@ if let unwrapped = gravityConstant {
     Try printing a car's price using a name that doesn't exist.
 */
 //: [Next](@next)
+let carPrices = ["Mazda3": 50000,
+                 "CRV": 60000,
+                 "Hummer": 100000]
+
+for (name, price) in carPrices{
+  print ("\(name) is $\(price)")
+}

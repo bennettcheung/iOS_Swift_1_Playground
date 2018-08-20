6ce796069fe2876eacee6e7ac47c072f2e3afa91
Bennett <bennettcheung@yahoo.com>
2018-08-20 13:14:11 -0700

    completed part 6

--- W4D1_Playground.playground/Pages/Functions and Closures.xcplaygroundpage/Contents.swift
+++ W4D1_Playground.playground/Pages/Functions and Closures.xcplaygroundpage/Contents.swift
@@ -42,23 +42,58 @@ func sayHello(toPerson: String) -> String{
  - Experiment:
  Try calling all of the functions above. They all have the same function name, but the compiler doesn't complain. Can you think of why this might be?
  */
-
+//different signature, so the complier knows
+sayHello()
 /*:
  - Experiment:
  Try creating your own function that accepts two parameters of any type you choose. Have the function print out the two parameters and test your function.
  */
+func saySomething(toPerson: String, fromPerson: String){
+  print ("To \(toPerson) from \(fromPerson)")
+}
 
+saySomething(toPerson: "Sam", fromPerson: "Bennett")
 /*:
  - Callout(Challenge):
  Create four separate functions to add, subtract, multiple, and divide with two parameters given to it and returns a number result. Try testing each one afterwards.
  
  */
+func sum(param1: Int, param2: Int)->Int{
+  return param1 + param2
+}
+func subtract(param1: Int, param2: Int)->Int{
+  return param1 - param2
+}
 
+func multiply(param1: Int, param2: Int)->Int{
+  return param1 * param2
+}
+
+func divide(param1: Int, param2: Int)->Int{
+  return param1 / param2
+}
+
+
+print (sum (param1: 2, param2: 5))
+print (subtract (param1: 2, param2: 5))
+print (multiply (param1: 2, param2: 5))
+print (divide (param1: 4, param2: 2))
 /*:
  - Callout(Challenge):
  Create your own 'reverse' function that takes in an array of Int, reverses the order of the array, and returns the newly reversed array of Int. The array class has its own 'reverse' method, but do not use it for this challenge.
  */
+func myReverse(inputArray: [Any])->[Any]{
+  var outputArray:[Any] = []
+  for index in (0..<inputArray.count).reversed(){
+    outputArray.append(inputArray[index])
+  }
+  
+ return outputArray
+}
 
+let cities = ["Vancouver", "Toronto", "montreal"]
+
+print(myReverse(inputArray: cities))
 /*:
  ## Closures
  
@@ -117,19 +152,32 @@ var sayHelloClosureWithReturn = { (name: String) -> String in
  - Experiment:
  Try calling all of the closures above. What do you notice that is different from calling a function?
  */
-
+sayHelloClosureWithReturn("Hello")
+//won't help with the parameter for input
 /*:
  - Experiment:
  Try creating your own closure that accepts two parameters of any type you choose. Have the closure print out the two parameters and test your closure.
  */
-
+var saySomethingclosure = {(string1: String, string2: String) -> () in
+  print("String 1 \(string1) and string 2 \(string2)")
+}
+saySomethingclosure("Say this", "say that")
 /*:
  - Experiment:
  Declare a variable with an explicit closure type: `(String) -> (String)`. This closure type says it takes one parameter of type String and returns a variable of type String.
  */
+var sayOneClosure = {(input: String) -> String in
+    return "something " + input
+}
 
+print (sayOneClosure("Pass this in"))
 /*:
  - Callout(Challenge):
  Create a closure with at least two parameters of your choice and decide whether or not it returns anything. Then create a function that takes in your closure as a parameter and one additional parameter of your choice.
  */
 //: [Next](@next)
+func takeInClosure(inputClosure: (String, String)-> (), anotherParam: String){
+  inputClosure("Inside take in Closure", anotherParam)
+}
+
+print (takeInClosure(inputClosure: saySomethingclosure, anotherParam: "inception"))

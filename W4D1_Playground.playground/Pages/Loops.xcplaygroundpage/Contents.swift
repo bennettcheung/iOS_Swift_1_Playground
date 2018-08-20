1315344e06874eb195c841b23854c3c1d061b8c5
Bennett <bennettcheung@yahoo.com>
2018-08-20 12:49:20 -0700

    part 5 completed

--- W4D1_Playground.playground/Pages/Loops.xcplaygroundpage/Contents.swift
+++ W4D1_Playground.playground/Pages/Loops.xcplaygroundpage/Contents.swift
@@ -29,20 +29,30 @@ for i in 2...5 {
  - Experiment:
  What's the difference between `2...5` vs `2..<5`?
  */
-
+// 2...5 go 2, 3, 4, 5
+//2..<5 go 2, 3, 4
 
 /*:
  - Experiment:
   Use a ranged for loop to search through the 'pets' array above for the word 'pig' and print out its index.
  */
-
+for index in 0..<pets.count{
+  if pets[index] == "pig"{
+    print ("index is \(index)")
+  }
+}
 
 /*:
  - Experiment:
  Create an array of random numbers of your choosing then make a for loop that adds 1 to each number.
  */
+var numbers = [3, 6, 8, 12, 16]
 
-
+for index in 0..<numbers.count{
+  numbers[index] = numbers[index] + 1;
+  
+}
+print (numbers)
 /*:
  - Experiment:
  Take a look at the example below. Modify the code so that it searches for both the largest and smallest number. Then print out the result.
@@ -58,23 +68,33 @@ let interestingNumbers = [
     "Square": [1, 4, 9, 16, 25],
 ]
 var largest = 0
+var smallest = 10000
 for (_, numbers) in interestingNumbers {
     for number in numbers {
         if number > largest {
             largest = number
         }
+      if number < smallest {
+        smallest = number
+      }
     }
 }
 print(largest)
-
+print (smallest)
 
 /*:
  - Callout(Challenge):
  Given a number `N`, from 0 to `N`, add up all the odd numbers and print out the result.
  ie: N = 5, 1+3+5 = 9
  */
-
-
+let Num = 5
+var sum = 0
+for index in 0...Num{
+  if index % 2 == 1{
+    sum += index
+  }
+}
+print (sum)
 /*:
  - Callout(Challenge):
  Given the following array of numbers, determine the frequency of each number using a for loop.
@@ -90,4 +110,18 @@ print(largest)
 
 let numberArray = [1, 4, 5, 5, 5, 3, 2, 1, 4, 2, 2, 2, 1]
 
+var frequency = [0: 0]
+
+for num in numberArray{
+  if let number = frequency[num] {
+    frequency[num] = number + 1
+  }
+  else{
+    frequency[num] = 1
+  }
+  
+}
+for (index, numberOfTimes) in frequency{
+  print ("\(index): Occurs \(numberOfTimes) times")
+}
 //: [Next](@next)
